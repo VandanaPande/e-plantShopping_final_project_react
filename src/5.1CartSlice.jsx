599e724bd6bd7c0308d8ed60ed3442355f6ea48b
VandanaPande <129248223+VandanaPande@users.noreply.github.com>
2025-10-15 18:12:44 +0530

    Create 5.1CartSlice.jsx

---
+++ src/5.1CartSlice.jsx
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+//remvedremveiteam
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    // Add a new plant or bump its quantity if it already exists
+    addItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find((item) => item.name === name);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+    },
+
+    /
+
+    // Update quantity of a specific plant
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
