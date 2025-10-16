9cfb14dcdbf1824693340b1b65aedcd6421d9a6d
VandanaPande <129248223+VandanaPande@users.noreply.github.com>
2025-10-16 12:41:12 +0530

    Update 5.1CartSlice.jsx

--- src/5.1CartSlice.jsx
+++ src/5.1CartSlice.jsx
@@ -7,24 +7,7 @@ export const CartSlice = createSlice({
   },
   reducers: {
     // Add a new plant or bump its quantity if it already exists
-    addItem: (state, action) => {
-      const { name, image, cost } = action.payload;
-      const existingItem = state.items.find((item) => item.name === name);
-      if (existingItem) {
-        existingItem.quantity += 1;
-      } else {
-        state.items.push({ name, image, cost, quantity: 1 });
-      }
-    },
-
-    /
-
-    // Update quantity of a specific plant
-    updateQuantity: (state, action) => {
-      const { name, quantity } = action.payload;
-      const itemToUpdate = state.items.find((item) => item.name === name);
-      if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
+    
       }
     },
   },
