38dd0023dc0a522d6330164fc6c78474c7c4dfff
VandanaPande <129248223+VandanaPande@users.noreply.github.com>
2025-10-17 11:01:28 +0530

    Create 3.3_3pointsCartItem.jsx

---
+++ src/3.3_3pointsCartItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './CartItem.css';
+
+const CartItem = () => {
+  const cart = useSelector(state => state.cart.items);
+
+  return (
+    <div className="cart-container">
+      {cart.map(item => (
+        <div className="cart-item" key={item.name}>
+          <div>{item.name}</div>
+          <div>{item.cost}</div>
+          {/* image missing */}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CartItem;
+ 
